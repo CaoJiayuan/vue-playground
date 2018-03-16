1bc16855942412e46d09262cf5d4a8e5e996cac4
cjy <cjy632258@hotmail.com>
2018-03-16 18:22:27 +0800

    update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,13 @@
 import App from './App.vue'
 import {router} from './router'
 import store from './store';
+import {Storage} from './app/utils'
 require('./app/request');
 require('./assets/style/app.sass')
 
 Vue.config.productionTip = false
 Vue.use(Vuetify)
+Vue.use(Storage)
 new Vue({
   el: `#${id}`,
   router,
